d0c3d4b26212ffdb0676efb6f050f83a31ecec46
Dmitrii Barilkin <dimabaril@gmail.com>
2024-01-19 19:15:22 +0100

    refactor: encoding option style

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const { stdout } = process;
 const filePath = path.join(__dirname, 'text.txt');
-const readFileStream = fs.createReadStream(filePath, 'utf8');
+const readFileStream = fs.createReadStream(filePath, { encoding: 'utf8' });
 
 readFileStream.on('data', (chunk) => stdout.write(chunk));
 readFileStream.on('error', (error) => stdout.write(error.message));
