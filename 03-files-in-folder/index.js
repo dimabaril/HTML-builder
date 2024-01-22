9621526c4c38d5e3b5868c3b69c55d1ad0a3641f
Dmitrii Barilkin <dimabaril@gmail.com>
2024-01-22 03:49:32 +0100

    refactor and clean: 03-files-in-folder now it's real async

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,41 +1,12 @@
-// const fs = require('fs');
-// const path = require('path');
-
-// const dirPath = path.join(__dirname, 'secret-folder');
-
-// fs.readdir(dirPath, (err, files) => {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     files.forEach((item) => {
-//       const filePath = path.join(dirPath, item);
-//       const nameInf = path.parse(filePath);
-//       fs.stat(filePath, (err, fileStat) => {
-//         if (err) {
-//           console.log(err);
-//         } else {
-//           if (fileStat.isFile()) {
-//             console.log(
-//               `${nameInf.name} - ${nameInf.ext.slice(1)} - ${convertBytes(
-//                 fileStat.size,
-//               )}`,
-//             );
-//           }
-//         }
-//       });
-//     });
-//   }
-// });
-
 const fs = require('fs').promises;
 const path = require('path');
 
 const dirPath = path.join(__dirname, 'secret-folder');
 
-async function listFiles(dirPath) {
+async function listFilesInfo(dirPath) {
   try {
     const files = await fs.readdir(dirPath);
-    for (const item of files) {
+    files.forEach(async (item) => {
       const filePath = path.join(dirPath, item);
       const nameInf = path.parse(filePath);
       const fileStat = await fs.stat(filePath);
@@ -46,68 +17,11 @@ async function listFiles(dirPath) {
           )}`,
         );
       }
-    }
+    });
   } catch (err) {
     console.error(err);
   }
 }
-
-// function listFiles(dirPath) {
-//   fs.readdir(dirPath)
-//     .then((files) => {
-//       files.forEach((item) => {
-//         const filePath = path.join(dirPath, item);
-//         const nameInf = path.parse(filePath);
-//         fs.stat(filePath)
-//           .then((fileStat) => {
-//             if (fileStat.isFile()) {
-//               console.log(
-//                 `${nameInf.name} - ${nameInf.ext.slice(1)} - ${convertBytes(
-//                   fileStat.size,
-//                 )}`,
-//               );
-//             }
-//           })
-//           .catch((err) => console.log(err));
-//       });
-//     })
-//     .catch((err) => console.log(err));
-// }
-
-// function getFileInfo(filePath) {
-//   const nameInf = path.parse(filePath);
-//   return fs
-//     .stat(filePath)
-//     .then((fileStat) => {
-//       if (fileStat.isFile()) {
-//         return {
-//           name: nameInf.name,
-//           ext: nameInf.ext.slice(1),
-//           size: convertBytes(fileStat.size),
-//         };
-//       }
-//     })
-//     .catch((err) => console.log(err));
-// }
-
-// function printFileInfo(fileInfo) {
-//   if (fileInfo) {
-//     console.log(`${fileInfo.name} - ${fileInfo.ext} - ${fileInfo.size}`);
-//   }
-// }
-
-// function listFiles(dirPath) {
-//   fs.readdir(dirPath)
-//     .then((files) => {
-//       files.forEach((item) => {
-//         const filePath = path.join(dirPath, item);
-//         getFileInfo(filePath).then(printFileInfo);
-//       });
-//     })
-//     .catch((err) => console.log(err));
-// }
-
-listFiles(dirPath);
 
 function convertBytes(bytes) {
   const sizes = ['bytes', 'kb', 'mb', 'gb', 'tb'];
@@ -117,3 +31,5 @@ function convertBytes(bytes) {
   const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
   return Math.round(bytes / Math.pow(1024, i)) + sizes[i];
 }
+
+listFilesInfo(dirPath);
